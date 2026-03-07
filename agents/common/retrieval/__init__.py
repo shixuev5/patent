b97bc072832cf0d6b64316c018f9196a1b1b2538
yanhao01 <yanhao01@qianxin.com>
2026-03-07 11:51:36 +0800

    feat: 专利分析添加RAG

--- agents/common/retrieval/__init__.py
+++ agents/common/retrieval/__init__.py
@@ -1,5 +1,6 @@
 """Retrieval toolkit with chunking, vector search, and rerank routing."""
 
 from .service import retrieve_segments, drop_retrieval_session, RetrievalService
+from .query_rewrite import QueryRewriteService
 
-__all__ = ["retrieve_segments", "drop_retrieval_session", "RetrievalService"]
+__all__ = ["retrieve_segments", "drop_retrieval_session", "RetrievalService", "QueryRewriteService"]
